277bac49629e98f98e0cb2bf5ca7c509744cb5f4
Sreyas Tendulkar <tendulkarsreyas1415@gmail.com>
2025-08-07 01:35:14 +0530

    made file destructure tf files

---
+++ hello-file-destructure/abc.tf
@@ -0,0 +1,13 @@
+output "abcoutputblock" {
+value = "this is abc hello world block"
+}
+
+#output the files are executed in alphabetical order
+# PS C:\Users\Sreyas Tendulkar\Desktop\myrepos> cd hello-file-destructure
+# PS C:\Users\Sreyas Tendulkar\Desktop\myrepos\hello-file-destructure> terraform plan                                           
+
+# Changes to Outputs:
+#   + abcoutputblock    = "this is abc hello world block"
+#   + firstoutputblock  = " this is first hello world block"
+#   + secondoutputblock = "this is second hello world block"
+#   + thirdoutputblock  = "this is third hello world block"
