7ddfa0aa2ff3838ae4af764e071d8292b5a99942
Sreyas Tendulkar <tendulkarsreyas1415@gmail.com>
2025-08-07 01:24:57 +0530

    Multi block terraform file to display different messages

---
+++ multi-block-hello-world/first.tf
@@ -0,0 +1,19 @@
+output "firstoutputblock" {
+    value = " this is first hello world block"
+}
+
+output "secondoutputblock"{
+    value = "this is second hello world block"
+
+}
+
+output "thirdoutputblock" {
+value = "this is third hello world block"
+}
+
+#this displays multiple display blocks in action
+
+# Changes to Outputs:
+#   + firstoutputblock  = " this is first hello world block"
+#   + secondoutputblock = "this is second hello world block"
+#   + thirdoutputblock  = "this is third hello world block"
