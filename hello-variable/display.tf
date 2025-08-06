47ecf267c3feb7a0b15043021ec758c1b13a8a92
Sreyas Tendulkar <tendulkarsreyas1415@gmail.com>
2025-08-07 02:26:38 +0530

    passing multiple params in command line

--- hello-variable/display.tf
+++ hello-variable/display.tf
@@ -1,13 +1,11 @@
 output "custom_hello"{
-    value = "hello, ${var.username}"
+    value = "hello, ${var.username}, your age is ${var.age}"
 
 }
 # interactive mode
 # PS C:\Users\Sreyas Tendulkar\Desktop\myrepos\hello-variable> terraform plan
 # var.username
 #   Enter a value: sreyas
-
-
 # Changes to Outputs:
 #   + custom_hello = "hello, sreyas"
 
@@ -15,6 +13,14 @@ output "custom_hello"{
 
 # non-interactive mode
 # PS C:\Users\Sreyas Tendulkar\Desktop\myrepos\hello-variable> terraform plan -var "username=tejas"
-
 # Changes to Outputs:
 #   + custom_hello = "hello, tejas"
+
+
+
+
+# made changes to variable to adjsut age variable with default value and pasing multiple perams via command line
+# PS C:\Users\Sreyas Tendulkar\Desktop\myrepos\hello-variable> terraform plan -var "username=tejas" -var "age=22"
+
+# Changes to Outputs:
+#   + custom_hello = "hello, tejas, your age is 22"
--- hello-variable/variable.tf
+++ hello-variable/variable.tf
@@ -1,4 +1,9 @@
 variable username{
+    default="sreyas"
+
+}
+variable age{
+    default=24
 
 }
 #variables files is stored in diff for destructing purposes
